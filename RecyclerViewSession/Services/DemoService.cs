856d1f11cabd4e23af5072c1579f992b3cd23f16
Joel Peterson <jpeters5392@gmail.com>
2016-09-24 19:08:54 -0500

    swapped out images

--- RecyclerViewSession/Services/DemoService.cs
+++ RecyclerViewSession/Services/DemoService.cs
@@ -47,18 +47,18 @@ namespace RecyclerViewSession.Services
 		void InitializeDataSet()
 		{
 			items = new List<DemoModel>();
-			PopulateModel("Basic List", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(MainActivity));
-			PopulateModel("Horizontal List", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(HorizontalListActivity));
-			PopulateModel("Basic List with Dividers", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/dan-edgar-116.png", typeof(DividerActivity));
-			PopulateModel("Horizontal List with Dividers", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(HorizontalListWithDividersActivity));
-			PopulateModel("List of Cards", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/brian-maula-100.png", typeof(BasicCardActivity));
-			PopulateModel("List of Swipeable Cards", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/brett-hazen-106.png", typeof(SwipeableCardActivity));
-			PopulateModel("Basic Grid", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(BasicGridActivity));
-			PopulateModel("Reversed Grid", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(ReversedGridActivity));
-			PopulateModel("Horizontal Grid", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(HorizontalGridActivity));
-			PopulateModel("Hide FAB", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/jason-erdahl-289.jpg", typeof(HideFabActivity));
-			PopulateModel("Disappearing Toolbar", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(DisappearingToolbarActivity));
-			PopulateModel("Resizing Toolbar", "https://ilmwebsitestorage.blob.core.windows.net/profile-images/joel-peterson-112.png", typeof(ResizeToolbarActivity));
+			PopulateModel("Basic List", "http://images.clipshrine.com/wheel/thumb-Number-1-66.6-3874.png", typeof(MainActivity));
+			PopulateModel("Horizontal List", "http://images.clipshrine.com/getimg/PngMedium-Number-2-3875.png", typeof(HorizontalListActivity));
+			PopulateModel("Basic List with Dividers", "http://images.clipshrine.com/download/wheel/medium-Number-3-0-3876.png", typeof(DividerActivity));
+			PopulateModel("Horizontal List with Dividers", "http://images.clipshrine.com/download/wheel/medium-Number-4-33.3-3878.png", typeof(HorizontalListWithDividersActivity));
+			PopulateModel("List of Cards", "http://images.clipshrine.com/wheel/thumb-Number-5-0-3879.png", typeof(BasicCardActivity));
+			PopulateModel("List of Swipeable Cards", "http://images.clipshrine.com/wheel/medium-Number-6-166.6-3880.png", typeof(SwipeableCardActivity));
+			PopulateModel("Basic Grid", "http://images.clipshrine.com/wheel/medium-Number-7-0-3881.png", typeof(BasicGridActivity));
+			PopulateModel("Reversed Grid", "http://images.clipshrine.com/download/wheel/medium-Number-8-33.3-3882.png", typeof(ReversedGridActivity));
+			PopulateModel("Horizontal Grid", "http://images.clipshrine.com/getimg/PngMedium-Number-9-3883.png", typeof(HorizontalGridActivity));
+			PopulateModel("Hide FAB", "https://openclipart.org/image/300px/svg_to_png/203546/number-10.png", typeof(HideFabActivity));
+			PopulateModel("Disappearing Toolbar", "http://justclassics.wikispaces.com/file/view/red-rounded-with-number-11-md.png/347475496/red-rounded-with-number-11-md.png", typeof(DisappearingToolbarActivity));
+			PopulateModel("Resizing Toolbar", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/12_white%2C_blue_rounded_rectangle.svg/325px-12_white%2C_blue_rounded_rectangle.svg.png", typeof(ResizeToolbarActivity));
 		}
 	}
 }
